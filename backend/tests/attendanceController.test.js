bc36bc24385e53d1565480080a4cb8e61da70531
Nazar <nazar123321nazargr@gamil.com>
2026-04-29 19:07:21 +0300

    added bug for settings test

--- backend/tests/attendanceController.test.js
+++ backend/tests/attendanceController.test.js
@@ -3,7 +3,7 @@ const Attendance = require('../models/Attendance');
 const Lesson = require('../models/Lesson');
 const Student = require('../models/Student');
 
-jest.mock('../models/Attendance', () => ({
+jest.mock('../models/Attefndance', () => ({
   findOneAndUpdate: jest.fn(),
 }));
 
